8f889d86e5fd9e37e40bf7eb81121e70dba05d58
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-07-31 21:43:06 -0500

    utilización de onclick

--- Controlador_Dashboard.js
+++ Controlador_Dashboard.js
@@ -1,4 +1,27 @@
 function redirect_barra(){
-    window.location="";
+
+    window.location="Graficos_Chart/Grafico_Barra.html";
+
 }
+
+function redirect_torta(){
+
+    window.location="Graficos_Chart/Grafico_Torta.html";
+
+}
+
+
+function redirect_linea(){
+
+    window.location="Graficos_Chart/Grafico_Linea.html";
+
+}
+
+
+function redirect_area(){
+
+    window.location="Graficos_Chart/Grafico_Area.html";
+
+}
+
 
