ee773eef46b97d8f774d4222066d5fd1e541e253
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-07-31 22:01:02 -0500

    aplicación del grafico de barra

--- Graficos_Chart/JS/Barra.js
+++ Graficos_Chart/JS/Barra.js
@@ -1,18 +1,27 @@
 
 function grafico_barra(){
 
+    //  donde se va a representar nuestro grafico
     const ctx = document.getElementById('myChart');
 
+   // inico de nuestro grafico
   new Chart(ctx, {
     type: 'bar',
+    // toda la informacion para representar el grafico
     data: {
+        // titulos de cada barra
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
+
         label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
+        // los datos segun los titulos establecidos
+        data: [12, 5, 3, 5, 2, 3],
+        borderWidth: 3,
+        backgroundColor:['#d41515ff','#1900fcff','#ffea04ff','#32f800ff', ' #6f10ebff','#eba610ff'],
+        borderColor: ['#000000ff'],
       }]
     },
+
     options: {
       scales: {
         y: {
