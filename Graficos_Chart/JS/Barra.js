948c4c63cdd46ba3435c71f95697123598aa211e
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-07-31 21:43:35 -0500

    inicio de implementación de grafico barra

---
+++ Graficos_Chart/JS/Barra.js
@@ -0,0 +1,29 @@
+
+function grafico_barra(){
+
+    const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+}
+
+
+grafico_barra() /* inicialización del js una vez se cargue la pagina html*/
+
