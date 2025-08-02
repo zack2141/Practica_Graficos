ecc20deb4deeb4f0b819efdb841839639823c4cc
zack2141 <wilmar.cardona7168@ucaldas.edu.co>
2025-08-01 20:23:24 -0500

    creacion de apartado de graficos de google y conexiones

--- Controlador_Dashboard.js
+++ Controlador_Dashboard.js
@@ -1,26 +1,55 @@
-function redirect_barra(){
+
+// funciones para redireccionar hacia los graficos de chartjs
+function redirect_barra_chart(){
 
     window.location="Graficos_Chart/Grafico_Barra.html";
 
 }
 
-function redirect_torta(){
+function redirect_torta_chart(){
 
     window.location="Graficos_Chart/Grafico_Torta.html";
 
 }
 
 
-function redirect_linea(){
+function redirect_linea_chart(){
 
     window.location="Graficos_Chart/Grafico_Linea.html";
 
 }
 
 
-function redirect_area(){
+function redirect_area_chart(){
 
     window.location="Graficos_Chart/Grafico_Area.html";
+
+}
+
+// funciones para redireccionar hacia los graficos de google
+function redirect_barra_google(){
+
+    window.location="Graficos_Google/Grafico_Barra.html";
+
+}
+
+function redirect_torta_google(){
+
+    window.location="Graficos_Google/Grafico_Torta.html";
+
+}
+
+
+function redirect_linea_google(){
+
+    window.location="Graficos_Google/Grafico_Linea.html";
+
+}
+
+
+function redirect_area_google(){
+
+    window.location="Graficos_Google/Grafico_Area.html";
 
 }
 
